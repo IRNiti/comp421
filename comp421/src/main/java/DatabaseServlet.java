9d1bce1a45e4a34ddf0b9509d1d0286f1f3c8cda
TANguyen1893 <ta.nguyen1893@gmail.com>
2016-03-19 19:46:09 -0400

    implemented create user and did error checking to see if email exists. also returns json data

--- comp421/src/main/java/DatabaseServlet.java
+++ comp421/src/main/java/DatabaseServlet.java
@@ -1,16 +1,18 @@
 package main.java;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import java.sql.* ;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import com.google.gson.Gson;
 /**
  * 
  * Servlet implementation class DatabaseServlet
@@ -27,10 +29,10 @@ public class DatabaseServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
         try {
         	DriverManager.registerDriver (new org.postgresql.Driver());
-            String url = "jdbc:postgresql://comp421.cs.mcgill.ca:5432/cs421";
-            connection = DriverManager.getConnection (url, null, null); // change both null values to username and password to connect to the db
-//			String url = "jdbc:postgresql://10.0.1.8:5432/CarRental";
-//			connection = DriverManager.getConnection(url, "pi", "nguyen");
+//            String url = "jdbc:postgresql://comp421.cs.mcgill.ca:5432/cs421";
+//            connection = DriverManager.getConnection (url, null, null); // change both null values to username and password to connect to the db
+			String url = "jdbc:postgresql://10.0.1.8:5432/CarRental";
+			connection = DriverManager.getConnection(url, "pi", "nguyen");
         } catch (Exception e){
         	e.printStackTrace();
         }
@@ -54,22 +56,71 @@ public class DatabaseServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		// Creating a user account: uid, name, address, email, isPremium, points
+		HttpSession session = request.getSession(true);
 		String name = request.getParameter("name");
 		String address = request.getParameter("address");
 		String email = request.getParameter("email");
 		String isPremium = request.getParameter("isPremium"); // on client side, the value should be either TRUE or FALSE, not 0 or 1
+		boolean premium;
+		if (isPremium.toLowerCase().equals("true")) {
+			premium = true;
+		} else {
+			premium = false;
+		}
 		int points = 0;
-		
 		String tableName = "\"cs421g04\"" + "." + "\"Users\"";
 		String insertStatement = String.format("INSERT INTO %s (name, address, email, \"isPremium\", points) VALUES ('%s', '%s', '%s', '%s', %d)", 
-				tableName, name, address, email, isPremium, points);
+				tableName, name, address, email, isPremium, points);		
 		try {
-			Statement statement = connection.createStatement();
-			ResultSet rs = statement.executeQuery(insertStatement);
+			User newUser = this.createUser(name, address, email, premium, points);
+			if (newUser != null) {
+				Statement statement = this.connection.createStatement();
+				statement.executeUpdate(insertStatement);
+				String jsonResponse = new Gson().toJson(newUser);
+				response.setContentType("application/json");
+				response.setCharacterEncoding("UTF-8");
+				response.getWriter().write(jsonResponse);
+			} else {
+				// error has occured due to entering the same email
+				User user = new User(-1, "", "", "", false, -1); // this cannot happen with a negative uid so front-end will need to check
+				String jsonResponse = new Gson().toJson(user);
+				response.setContentType("application/json");
+				response.setCharacterEncoding("UTF-8");
+				response.getWriter().write(jsonResponse);
+			}	
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private User createUser(String name, String address, String email, boolean isPremium, int points) {
+		// need to make 
+		String tableName = "\"cs421g04\"" + "." + "\"Users\"";
+		String getUID = String.format("SELECT * from %s WHERE email='%s'", tableName, email);
+		String getNextUserId = String.format("select nextval('unique_userid')");
+		int emailCount = 0;
+		int userCount = 0;
+		try {
+			Statement statement = this.connection.createStatement();
+			ResultSet rs = statement.executeQuery(getUID);
+			while (rs.next()) {
+				emailCount++;
+			}
+			if (emailCount != 0) {
+				return null;
+			}
+			statement = this.connection.createStatement();
+			rs = statement.executeQuery(getNextUserId);
+			while (rs.next()) {
+				userCount = rs.getInt("nextval");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// using the same email is not allowed
 
+		User user = new User(userCount, name, address, email, isPremium, points);
+		return user;
 	}
 
 }
---
+++ comp421/src/main/java/User.java
@@ -0,0 +1,73 @@
+package main.java;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+	
+	private int uId;
+	private String name;
+	private String address;
+	private String email;
+	private boolean isPremium;
+	private int points;
+	
+	
+	public User(int id, String name, String address, String email, boolean isPremium, int points) {
+		this.uId = id;
+		this.name = name;
+		this.address = address;
+		this.email = email;
+		this.isPremium = isPremium;
+		this.points = points;
+	}
+	
+	public int getUID() {
+		return this.uId;
+	}
+	
+	public void setUID(int id) {
+		this.uId = id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return this.address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public boolean isPremium() {
+		return this.isPremium;
+	}
+	
+	public void setPremium(boolean premium) {
+		this.isPremium = premium;
+	}
+	
+	public int getPoints() {
+		return this.points;
+	}
+	
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+
+}
