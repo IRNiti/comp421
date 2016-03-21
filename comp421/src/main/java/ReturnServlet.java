d75622e9d9d1ab9d4e3897cee9b6212f2880f256
IRNiti <ioana.niti@mail.mcgill.ca>
2016-03-21 01:43:48 -0400

    return car servlet

---
+++ comp421/src/main/java/ReturnServlet.java
@@ -0,0 +1,162 @@
+package main.java;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class ReturnServlet
+ */
+public class ReturnServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection;
+	private Statement statement;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ReturnServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+		try {
+			DriverManager.registerDriver (new org.postgresql.Driver());
+			String url = "jdbc:postgresql://comp421.cs.mcgill.ca:5432/cs421";
+			connection = DriverManager.getConnection (url, "cs421g04", "CarRental#1"); // change both null values to username and password to connect to the db
+			//			String url = "jdbc:postgresql://10.0.1.8:5432/CarRental";
+			//			connection = DriverManager.getConnection(url, "pi", "nguyen");
+			statement = connection.createStatement ( ) ;
+
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		String rID = request.getParameter("reservationID");
+		String branchAddress = request.getParameter("branchAddress");
+		int bId = this.getBranchId(branchAddress);
+		int queryBranch = -1;
+		String message = "";
+		String jsonResponse = "";
+
+
+		// check if entered branch is same as the one associated to rID in Dropoff table				
+		try{
+			String querySQL = "SELECT \"bID\" FROM \"cs421g04\".\"Dropoff\" WHERE \"rID\" ="+rID;
+			java.sql.ResultSet rs = statement.executeQuery ( querySQL ) ;
+
+			while ( rs.next ( ) ) {
+				queryBranch = rs.getInt ( 1 ) ;
+			}
+
+			if(queryBranch != bId){
+				message = "mismatch from dropoff branch. Check original reservation";
+				jsonResponse = new Gson().toJson(message);
+				response.setContentType("application/json");
+				response.setCharacterEncoding("UTF-8");
+				response.getWriter().write(jsonResponse);
+				return;
+			}
+
+		} catch(SQLException e)
+		{
+
+			message = e.getMessage();
+			jsonResponse = new Gson().toJson(message);
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			response.getWriter().write(jsonResponse);
+			return;
+
+		}
+
+
+		//update vehicle branch to new branch
+		if(queryBranch == bId){
+			try {
+				String insertSQL = "UPDATE \"cs421g04\".\"Vehicles\" SET \"bID\" = "+bId;
+
+				System.out.println ( insertSQL ) ;
+				statement.executeUpdate ( insertSQL ) ;
+				message = "updated vehicle branch to "+bId;
+
+			} catch (SQLException e)
+			{
+				message = e.getMessage();
+				jsonResponse = new Gson().toJson(message);
+				response.setContentType("application/json");
+				response.setCharacterEncoding("UTF-8");
+				response.getWriter().write(jsonResponse);
+				return;
+			}
+
+			// set the vehicle as being returned
+			try {
+				String updateSQL = "UPDATE \"cs421g04\".\"Reservations\" SET \"isReturned\" = true WHERE \"rID\" = "+rID;
+
+				statement.executeUpdate ( updateSQL ) ;  
+
+			} catch (SQLException e)
+			{
+				message = e.getMessage();
+				jsonResponse = new Gson().toJson(message);
+				response.setContentType("application/json");
+				response.setCharacterEncoding("UTF-8");
+				response.getWriter().write(jsonResponse);
+				return;
+			}
+
+		}
+
+
+
+		// return json response to front-end
+		jsonResponse = new Gson().toJson(message);
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(jsonResponse);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	private int getBranchId(String address) {
+		String tableName = "\"cs421g04\"" + "." + "\"Branches\"";
+		String getBranchId = String.format("SELECT \"bID\" FROM %s WHERE address='%s'", tableName, address);
+		int id = -1;
+		try {
+			Statement statement = this.connection.createStatement();
+			ResultSet rs = statement.executeQuery(getBranchId);
+			while (rs.next()) {
+				id = rs.getInt("bID");
+				return id;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return id;
+	}
+
+
+}
--- comp421/src/main/java/ReviewServlet.java
+++ comp421/src/main/java/ReviewServlet.java
@@ -1,6 +1,6 @@
-	package main.java;
-	
-	import java.io.IOException;
+package main.java;
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -10,91 +10,80 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-	
 
-	import javax.servlet.ServletException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-	
+
 
 import com.google.gson.Gson;
-	
+
+/**
+ * Servlet implementation class ReviewServlet
+ */
+public class ReviewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection;
+	private Statement statement;
+
 	/**
-	 * Servlet implementation class ReviewServlet
+	 * @see HttpServlet#HttpServlet()
 	 */
-	public class ReviewServlet extends HttpServlet {
-		private static final long serialVersionUID = 1L;
-		private Connection connection;
-		private Statement statement;
-	       
-	    /**
-	     * @see HttpServlet#HttpServlet()
-	     */
-	    public ReviewServlet() {
-	        super();
-	        // TODO Auto-generated constructor stub
-	        try {
-	        	DriverManager.registerDriver (new org.postgresql.Driver());
-	        	
-	//			String url = "jdbc:postgresql://10.0.1.8:5432/CarRental";
-	//			connection = DriverManager.getConnection(url, "pi", "nguyen");
-	            statement = connection.createStatement ( ) ;
-	            
-	        } catch (Exception e){
-	        	e.printStackTrace();
-	        }
-	    }
-	    
-	    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			// TODO Auto-generated method stub
-	    	
-	    	
-	    	 int sqlCode=0;      // Variable to hold SQLCODE
-	         String sqlState="00000";  // Variable to hold SQLSTATE
-	         String message = "";
-	         
-			String vID = request.getParameter("vehicleID");
-			
-			String userName = request.getParameter("userName");
-	    	String review = "'"+request.getParameter("review")+"'";
-	    	
-	    	DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-	    	Date date = new Date();
-	    	String dbDate = "'"+dateFormat.format(date)+"'"; 
-	    	
-	    	String rating = request.getParameter("rating");
-			
-			String tableName = "\"cs421g04\"" + "." + "\"Review\"";
-				
-			try {
-			    String insertSQL = "INSERT INTO " + tableName +
-			    	" VALUES ("+vID+", "+"'"+userName+"'"+", "+review+", "+dbDate+", "+rating+")";
-	
-			    System.out.println ( insertSQL ) ;
-			    statement.executeUpdate ( insertSQL ) ;
-			    System.out.println ( "DONE" ) ;
-			    message = "DONE";
-	
-			    
-	
-			} catch (SQLException e)
-		            {
-		                sqlCode = e.getErrorCode(); // Get SQLCODE
-		                sqlState = e.getSQLState(); // Get SQLSTATE
-		                message = e.getMessage();
-		                
-		                // Your code to handle errors comes here;
-		                // something more meaningful than a print would be good
-		                System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
-		                System.out.println("Message: " + message);
-		            }
-				// return json response to front-end
-				String jsonResponse = new Gson().toJson(message);
-				response.setContentType("application/json");
-				response.setCharacterEncoding("UTF-8");
-				response.getWriter().write(jsonResponse);
-			
+	public ReviewServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+		try {
+			DriverManager.registerDriver (new org.postgresql.Driver());
+
+			//			String url = "jdbc:postgresql://10.0.1.8:5432/CarRental";
+			//			connection = DriverManager.getConnection(url, "pi", "nguyen");
+			statement = connection.createStatement ( ) ;
+
+		} catch (Exception e){
+			e.printStackTrace();
 		}
-	
 	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		String message = "";
+
+		String vID = request.getParameter("vehicleID");
+
+		String userName = request.getParameter("userName");
+		String review = "'"+request.getParameter("review")+"'";
+
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		String dbDate = "'"+dateFormat.format(date)+"'"; 
+
+		String rating = request.getParameter("rating");
+
+		String tableName = "\"cs421g04\"" + "." + "\"Review\"";
+
+		try {
+			String insertSQL = "INSERT INTO " + tableName +
+					" VALUES ("+vID+", "+"'"+userName+"'"+", "+review+", "+dbDate+", "+rating+")";
+
+			statement.executeUpdate ( insertSQL ) ;
+			message = "DONE";
+
+
+
+		} catch (SQLException e)
+		{
+			message = e.getMessage();
+
+		}
+		// return json response to front-end
+		String jsonResponse = new Gson().toJson(message);
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(jsonResponse);
+
+	}
+
+}
